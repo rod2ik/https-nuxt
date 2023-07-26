4a4dbd24030817de2f4db8faff49ea3d588f601a
rod2ik <rod2ik.dev@gmail.com>
2023-07-26 16:48:58 +0200

    fist commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# https-Nuxt 3
+
+Demo repo to fix the `https` devServer bug.
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  ssr: false,
+  devServer: {
+    https: {
+      key: './myserver+3-key.pem',
+      cert: './myserver+3.pem',
+    }
+  }
+})
