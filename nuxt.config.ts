63902ca8b6fd38577efa28bab15dcbeacb2ac387
rod2ik <rod2ik.dev@gmail.com>
2023-07-28 17:28:18 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
+  // ssr: false,
   devServer: {
     https: {
       key: './myserver+3-key.pem',
